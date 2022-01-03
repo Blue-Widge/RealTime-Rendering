eee1e98faf11ef801273ce9c0c99b2256d7bf08b
Blue-Widge <59555837+Blue-Widge@users.noreply.github.com>
2022-01-03 05:41:15 +0100

    Basic shader added

---
+++ RealTimeRendering/Matrix.c
@@ -0,0 +1,283 @@
+﻿#include "Matrix.h"
+#include "Vector.h"
+#include "Tools.h"
+
+const Mat3 Mat3_Identity = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+const Mat4 Mat4_Identity = {
+    1.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 1.0f
+};
+
+//-------------------------------------------------------------------------------------------------
+// Fonctions à coder
+
+// TODO
+Mat3 Mat3_MulMM(Mat3 mat1, Mat3 mat2)
+{
+    Mat3 result = { 0 };
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            for (int o = 0; o < 3; o++)
+                result.data[i][j] += mat1.data[i][o] * mat2.data[o][j];
+        }
+    }
+    return result;
+}
+
+
+// TODO
+Vec3 Mat3_MulMV(Mat3 mat, Vec3 v)
+{
+    Vec3 result = { 0 };
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            mat.data[i][j] *= v.data[j];
+            result.data[i] += mat.data[i][j];
+        }
+    }
+    return result;
+}
+
+// TODO
+float Mat3_Det(Mat3 mat)
+{
+    float detFirstPart = (mat.data[0][0] * mat.data[1][1] * mat.data[2][2])
+        + (mat.data[0][1] * mat.data[1][2] * mat.data[2][0])
+        + (mat.data[1][0] * mat.data[2][1] * mat.data[0][2]);
+
+    float detSecondPart = (mat.data[2][0] * mat.data[1][1] * mat.data[0][2])
+        + (mat.data[1][0] * mat.data[0][1] * mat.data[2][2])
+        + (mat.data[2][1] * mat.data[1][2] * mat.data[0][0]);
+
+    return detFirstPart - detSecondPart;
+}
+
+// TODO
+Mat4 Mat4_Transpose(Mat4 matrix)
+{
+    Mat4 copy = matrix;
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+            matrix.data[i][j] = copy.data[j][i];
+    }
+    return matrix;
+}
+
+// TODO
+Mat4 Mat4_MulMM(Mat4 mat1, Mat4 mat2)
+{
+    Mat4 result = { 0 };
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            for (int o = 0; o < 4; o++)
+                result.data[i][j] += mat1.data[i][o] * mat2.data[o][j];
+        }
+    }
+    return result;
+}
+// TODO
+Vec4 Mat4_MulMV(Mat4 mat, Vec4 v)
+{
+    Vec4 result = { 0 };
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            mat.data[i][j] *= v.data[j];
+            result.data[i] += mat.data[i][j];
+        }
+    }
+    return result;
+}
+
+// TODO
+Mat4 Mat4_Scale(Mat4 mat, float s)
+{
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+            mat.data[i][j] *= s;
+    }
+    return mat;
+}
+
+// TODO
+float Mat4_Cofactor(Mat4 mat, int i, int j)
+{
+    int sum = i + j;
+    Mat3 matrix = { 0 };
+    int u = 0, v = 0;
+
+    for (int o = 0; o < 4; ++o)
+    {
+        v = 0;
+        if (o == i)
+            continue;
+        for (int p = 0; p < 4; ++p)
+        {
+            if (p == j)
+                continue;
+            matrix.data[u][v] = mat.data[o][p];
+            v++;
+        }
+        u++;
+    }
+
+    if (sum % 2 == 0)
+        return Mat3_Det(matrix);
+    return Mat3_Det(matrix) * -1;
+}
+
+// TODO
+Mat4 Mat4_CofactorMatrix(Mat4 mat)
+{
+    Mat4 result = { 0 };
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            result.data[i][j] = Mat4_Cofactor(mat, i, j);
+        }
+    }
+    return result;
+}
+
+// TODO
+float Mat4_Det(Mat4 mat)
+{
+    return mat.data[3][3] * Mat4_Cofactor(mat, 3, 3);
+}
+
+// TODO
+Mat4 Mat4_Inv(Mat4 mat)
+{
+    float determinant = Mat4_Det(mat);
+    if (!determinant)
+        return Mat4_Identity;
+    return Mat4_Scale(Mat4_Transpose(Mat4_CofactorMatrix(mat)), 1.f / determinant);
+}
+
+//-------------------------------------------------------------------------------------------------
+// Fonctions du basecode
+
+Mat4 Mat4_GetProjectionMatrix(float l, float r, float b, float t, float n, float f)
+{
+    Mat4 projectionMatrix = Mat4_Identity;
+
+    if (r == l || t == b || f == n)
+        return projectionMatrix;
+
+    n = -n;
+    f = -f;
+
+    projectionMatrix.data[0][0] = 2.f * n / (r - l);
+    projectionMatrix.data[1][1] = 2.f * n / (t - b);
+    projectionMatrix.data[2][2] = (f + n) / (f - n);
+    projectionMatrix.data[3][3] = 0.f;
+
+    projectionMatrix.data[0][2] = -(r + l) / (r - l);
+    projectionMatrix.data[1][2] = -(t + b) / (t - b);
+    projectionMatrix.data[3][2] = 1.f;
+
+    projectionMatrix.data[2][3] = -2.f * f * n / (f - n);
+
+    return projectionMatrix;
+}
+
+
+Mat4 Mat4_GetScaleMatrix(float s)
+{
+    Mat4 scaleMatrix = Mat4_Identity;
+
+    for (int i = 0; i < 3; i++)
+        scaleMatrix.data[i][i] = s;
+
+    return scaleMatrix;
+}
+
+Mat4 Mat4_GetScale3Matrix(Vec3 v)
+{
+    Mat4 scaleMatrix = Mat4_Identity;
+
+    for (int i = 0; i < 3; i++)
+        scaleMatrix.data[i][i] = v.data[i];
+
+    return scaleMatrix;
+}
+
+Mat4 Mat4_GetTranslationMatrix(Vec3 v)
+{
+    Mat4 translationMatrix = Mat4_Identity;
+
+    for (int i = 0; i < 3; i++)
+    {
+        translationMatrix.data[i][3] = v.data[i];
+    }
+    translationMatrix.data[3][3] = 1.f;
+
+    return translationMatrix;
+}
+
+Mat4 Mat4_GetXRotationMatrix(float angle)
+{
+    Mat4 rotationMatrix = Mat4_Identity;
+
+    angle = (float)(angle * M_PI / 180.f);
+    float c = cosf(angle);
+    float s = sinf(angle);
+
+    rotationMatrix.data[1][1] = c;
+    rotationMatrix.data[2][2] = c;
+
+    rotationMatrix.data[1][2] = -s;
+    rotationMatrix.data[2][1] = s;
+
+    return rotationMatrix;
+}
+
+Mat4 Mat4_GetYRotationMatrix(float angle)
+{
+    Mat4 rotationMatrix = Mat4_Identity;
+
+    angle = (float)(angle * M_PI / 180.f);
+    float c = cosf(angle);
+    float s = sinf(angle);
+
+    rotationMatrix.data[0][0] = c;
+    rotationMatrix.data[2][2] = c;
+
+    rotationMatrix.data[2][0] = -s;
+    rotationMatrix.data[0][2] = s;
+
+    return rotationMatrix;
+}
+
+Mat4 Mat4_GetZRotationMatrix(float angle)
+{
+    Mat4 rotationMatrix = Mat4_Identity;
+
+    angle = (float)(angle * M_PI / 180.f);
+    float c = cosf(angle);
+    float s = sinf(angle);
+
+    rotationMatrix.data[0][0] = c;
+    rotationMatrix.data[1][1] = c;
+
+    rotationMatrix.data[0][1] = -s;
+    rotationMatrix.data[1][0] = s;
+
+    return rotationMatrix;
+}
---
+++ RealTimeRendering/Vector.c
@@ -0,0 +1,173 @@
+﻿#include "Vector.h"
+
+const Vec2 Vec2_Right = { +1.0f, +0.0f };
+const Vec2 Vec2_Left  = { -1.0f, +0.0f };
+const Vec2 Vec2_Up    = { +0.0f, +1.0f };
+const Vec2 Vec2_Down  = { +0.0f, -1.0f };
+const Vec2 Vec2_Zero  = { +0.0f, +0.0f };
+const Vec2 Vec2_One   = { +1.0f, +1.0f };
+
+const Vec3 Vec3_Right = { +1.0f, +0.0f, +0.0f };
+const Vec3 Vec3_Left  = { -1.0f, +0.0f, +0.0f };
+const Vec3 Vec3_Up    = { +0.0f, +1.0f, +0.0f };
+const Vec3 Vec3_Down  = { +0.0f, -1.0f, +0.0f };
+const Vec3 Vec3_Front = { +0.0f, +0.0f, +1.0f };
+const Vec3 Vec3_Back  = { +0.0f, +0.0f, -1.0f };
+const Vec3 Vec3_Zero  = { +0.0f, +0.0f, +0.0f };
+const Vec3 Vec3_One   = { +1.0f, +1.0f, +1.0f };
+
+const Vec4 Vec4_RightH = { +1.0f, +0.0f, +0.0f, +1.0f };
+const Vec4 Vec4_LeftH  = { -1.0f, +0.0f, +0.0f, +1.0f };
+const Vec4 Vec4_UpH    = { +0.0f, +1.0f, +0.0f, +1.0f };
+const Vec4 Vec4_DownH  = { +0.0f, -1.0f, +0.0f, +1.0f };
+const Vec4 Vec4_FrontH = { +0.0f, +0.0f, +1.0f, +1.0f };
+const Vec4 Vec4_BackH  = { +0.0f, +0.0f, -1.0f, +1.0f };
+const Vec4 Vec4_ZeroH  = { +0.0f, +0.0f, +0.0f, +1.0f };
+const Vec4 Vec4_Zero   = { +0.0f, +0.0f, +0.0f, +0.0f };
+const Vec4 Vec4_One    = { +1.0f, +1.0f, +1.0f, +1.0f };
+
+//-------------------------------------------------------------------------------------------------
+// Fonctions à coder
+
+// TODO
+Vec2 Vec2_Add(Vec2 v1, Vec2 v2)
+{
+    v1.x += v2.x;
+    v1.y += v2.y;
+    return v1;
+}
+
+
+// TODO
+Vec2 Vec2_Sub(Vec2 v1, Vec2 v2)
+{
+    v1.x -= v2.x;
+    v1.y -= v2.y;
+    return v1;
+}
+
+
+// TODO
+float Vec2_SignedArea(const Vec2 a, const Vec2 b, const Vec2 c)
+{
+    Vec2 x = Vec2_Sub(b, a);
+    Vec2 y = Vec2_Sub(c, a);
+    return (x.x * y.y) - (x.y * y.x);
+}
+
+// TODO
+bool Vec2_Barycentric(Vec2* vertices, Vec2 p, float* bary)
+{
+    Vec2 a = vertices[0];
+    Vec2 b = vertices[1];
+    Vec2 c = vertices[2];
+
+    Vec2 bmp = Vec2_Sub(b, p);
+    Vec2 cmp = Vec2_Sub(c, p);
+    Vec2 bma = Vec2_Sub(b, a);
+    Vec2 cma = Vec2_Sub(c, a);
+    Vec2 amp = Vec2_Sub(a, p);
+
+    float bas = (bma.x * cma.y) - (bma.y * cma.x);
+
+    bary[0] = ((bmp.x * cmp.y) - (bmp.y * cmp.x)) / bas;
+    bary[1] = ((cmp.x * amp.y) - (cmp.y * amp.x)) / bas;
+    bary[2] = ((amp.x * bmp.y) - (amp.y * bmp.x)) / bas;
+    return (bary[0] > 0 && bary[1] > 0 && bary[2] > 0) ? true : false;
+}
+
+// TODO
+Vec3 Vec3_Add(Vec3 v1, Vec3 v2)
+{
+    v1.x += v2.x;
+    v1.y += v2.y;
+    v1.z += v2.z;
+    return v1;
+}
+
+// TODO
+Vec3 Vec3_Sub(Vec3 v1, Vec3 v2)
+{
+    v1.x -= v2.x;
+    v1.y -= v2.y;
+    v1.z -= v2.z;
+    return v1;
+}
+
+// TODO
+Vec3 Vec3_Scale(Vec3 v, float s)
+{
+    v.x *= s;
+    v.y *= s;
+    v.z *= s;
+    return v;
+}
+
+// TODO
+float Vec3_Dot(Vec3 v1, Vec3 v2)
+{
+    return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+}
+
+// TODO
+Vec3 Vec3_Cross(Vec3 v1, Vec3 v2)
+{
+    Vec3 cross = { 0 };
+    cross.x = (v1.y * v2.z) - (v1.z * v2.y);
+    cross.y = (v1.z * v2.x) - (v1.x * v2.z);
+    cross.z = (v1.x * v2.y) - (v1.y * v2.x);
+    return cross;
+}
+
+// TODO
+float Vec3_Length(Vec3 v)
+{
+    return sqrtf((v.x * v.x) + (v.y * v.y) + (v.z * v.z));
+}
+
+// TODO
+Vec3 Vec3_Normalize(Vec3 v)
+{
+    float norme = Vec3_Length(v);
+    if (norme == 1)
+        return v;
+    return Vec3_Scale(v, 1.f / norme);
+}
+
+// TODO
+Vec3 Vec3_From4(Vec4 v)
+{
+    Vec3 current = { 0 };
+    v.w = v.w == 0 ? 1 : v.w;
+    current.x = v.x / v.w;
+    current.y = v.y / v.w;
+    current.z = v.z / v.w;
+    return current;
+}
+
+//-------------------------------------------------------------------------------------------------
+// Fonctions du basecode
+
+Vec3 Vec3_Mul(Vec3 v1, Vec3 v2)
+{
+    v1.x *= v2.x;
+    v1.y *= v2.y;
+    v1.z *= v2.z;
+    return v1;
+}
+
+Vec3 Vec3_Max(Vec3 v1, Vec3 v2)
+{
+    v1.x = fmaxf(v1.x, v2.x);
+    v1.y = fmaxf(v1.y, v2.y);
+    v1.z = fmaxf(v1.z, v2.z);
+    return v1;
+}
+
+Vec3 Vec3_Min(Vec3 v1, Vec3 v2)
+{
+    v1.x = fminf(v1.x, v2.x);
+    v1.y = fminf(v1.y, v2.y);
+    v1.z = fminf(v1.z, v2.z);
+    return v1;
+}
